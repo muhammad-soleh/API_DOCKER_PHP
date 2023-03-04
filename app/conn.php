3f6846b9e6ce2cf726e9dac2c5f5b79c7992d32b
muhammad-soleh <67421499+muhammad-soleh@users.noreply.github.com>
2023-03-04 16:23:26 +0700

    Update conn.php

--- app/conn.php
+++ app/conn.php
@@ -1,12 +1,7 @@
 <?php
-// $host = getenv('DB_HOST');
-// $username = getenv('DB_USER');
-// $password = getenv('DB_PASS');
-// $database = getenv('DB_NAME');
-
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'api_coba';
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$database = getenv('DB_NAME');
 
 $connection = mysqli_connect($host, $username, $password, $database);
